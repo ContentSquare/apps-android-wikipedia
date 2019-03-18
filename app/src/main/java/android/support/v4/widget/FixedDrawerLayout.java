e9d390f3ae0a8d3cb653aff1d63aac92986e2321
Kailin Huang <kailin.huang@contentsquare.com>
2019-03-18 15:32:46 +0100

    Migrating Wikipedia to AndroidX (#1)
    * Refactor
    
    * Gradle refactor and adding CS SDK
    
    * Remove unsupported code

--- app/src/main/java/android/support/v4/widget/FixedDrawerLayout.java
+++ app/src/main/java/android/support/v4/widget/FixedDrawerLayout.java
@@ -2,14 +2,14 @@ package android.support.v4.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
+
+import androidx.drawerlayout.widget.DrawerLayout;
 
 /**
  * TODO: Remove this class when Google updates the Support library.
  * This solves an intermittent crash when using DrawerLayout.
  *
  * https://code.google.com/p/android/issues/detail?id=77926
- *
  */
 public class FixedDrawerLayout extends DrawerLayout {
     public FixedDrawerLayout(Context context) {
@@ -23,12 +23,5 @@ public class FixedDrawerLayout extends DrawerLayout {
     public FixedDrawerLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
-
-    @Override
-    boolean isContentView(View child) {
-        if (child == null) {
-            return false;
-        }
-        return super.isContentView(child);
-    }
+    
 }
